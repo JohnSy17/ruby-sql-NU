43cd7dd76006b246228bd2f44fca5d6d9d769be0
JohnSy17 <john.syme@kellogg.northwestern.edu>
2026-02-03 16:29:26 +0000

    Models update

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -9,18 +9,61 @@ Company.destroy_all
 # **************************
 
 # - Insert, read, update, and delete rows in companies table
-#   (i.e. full CRUD of company data).
+#   (i.e. full CRUD of company data). Create, Read, Update, Delete
 
 # 1a. check out the schema file
 # 1b. check out the model file
 
+# cat log/development.log will allow us to see the log file.
 # 2. insert new rows in companies table
+new_company = Company.new #creates a new instance of the class
+# puts new_company
+# p new_company
+new_company["name"] = "Surridge Industries"
+new_company["city"] = "Chicago"
+new_company["state"] = "California"
+new_company["url"] = "www.google.com"
+# p new_company
+new_company.save
+# p new_company
+
+new_company = Company.new #creates a new instance of the class
+# puts new_company
+# p new_company
+new_company["name"] = "Amazon"
+new_company["city"] = "Seattle"
+new_company["state"] = "Washington"
+new_company["url"] = "www.amazon.com"
+# p new_company
+new_company.save
+# p new_company
+
+new_company = Company.new #creates a new instance of the class
+# puts new_company
+# p new_company
+new_company["name"] = "Apple"
+new_company["city"] = "Cupertino"
+new_company["state"] = "California"
+new_company["url"] = "www.apple.com"
+# p new_company
+new_company.save
+# p new_company
+
+# puts "Companies in db: #{Company.all.count}"
 
 # 3. query companies table to find all row with California company
 
+cali_companies = Company.where({"state" => "California"})
+# p cali_companies
+puts "Number of Cali Companies: #{cali_companies.count}"
+
 # 4. query companies table to find single row for Apple
+apple_info = Company.find_by({"name" => "Apple"}) #where statement always returns an array
+p apple_info 
 
 # 5. read a row's column value
+puts apple_info["url"]
+
 
 # 6. update a row's column value
 
