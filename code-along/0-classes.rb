ef655ecdcb6db2538fc30398190afab6fa046093
JohnSy17 <john.syme@kellogg.northwestern.edu>
2026-02-03 14:53:01 +0000

    Updated classes in the code along

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -2,7 +2,24 @@
 # when running this code.  To do so, your current working directory
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner code-along/0-classes.rb
+puts "hi"
 
 # String Class
+John_Fav_Food = "pizza"
+# puts John_Fav_Food.class #tells the class of the variable
+# puts John_Fav_Food.methods
+puts John_Fav_Food.upcase
+
 
 # String Methods
+
+class Dog
+  
+  def speak
+    puts "Woooooooof!"
+  end
+
+end
+
+fido = Dog.new #builds a new dog called fido. A way to run the class.
+puts fido.speak #call the method within the class dog called fido
